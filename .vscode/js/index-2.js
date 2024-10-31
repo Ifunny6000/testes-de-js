793199555954b62bc84f7b6cf38804f550560627
Gustavo M Assis <guga12assismoreira@gmail.com>
2024-10-31 08:03:50 -0300

    primeiro commit js-teste

---
+++ .vscode/js/index-2.js
@@ -0,0 +1,45 @@
+// let jogo1 ={
+//     nome: 'Final Fantasy'
+// }
+
+// let jogo2 = {
+//     nome: 'Fallout4'
+// }
+
+// let videoGame = {
+//     nome: 'Xbox',
+//     valor: 3000,
+//     jogos: [ 
+//         { nome: 'Final Fanta'}, 
+//         {nome:'Fallout'}]
+// }
+
+// let jogo3 = {
+//     nome: 'Fifa'
+// }
+
+// videoGame.jogos.push(jogo3)
+
+// console.log(videoGame)
+
+// let cliente = {
+//     nome: 'Ricardo',
+//     ultimoPedido: {
+//         produto: 'Xbox',
+//         valor: 3000,
+//         jogos: [
+//             { nome: 'Fifa'}
+//         ]
+//     }
+// }
+
+
+// console.log(cliente.ultimoPedido.jogos[0].nome)
+
+
+
+
+
+
+
+
---
+++ .vscode/js/index-3.js
@@ -0,0 +1,18 @@
+// console.log(document)
+
+// let p = document.getElementById('paragrafo')
+
+// console.log(p)
+
+
+// // let inpultPoClass = document.getElementsByClassName('tag-de-inpult');
+
+// console.log(inpultPoClass)
+
+// let inpultPoTag = document.getElementsByTagName('input');
+
+// console.log(inpultPoTag)
+
+// let inpultPorNome = document.querySelectorAll("inpult[name='email']");
+
+// console.log(inpultPorNome)
